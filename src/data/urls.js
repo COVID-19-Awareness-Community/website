f922f8a6e66042665e89932fb416a4add07910f0
Monarch Wadia <monarch@zeroprojects.ca>
2020-03-21 23:33:53 -0400

    Build

--- src/data/urls.js
+++ src/data/urls.js
@@ -3,5 +3,10 @@ export default [
     url: "https://www.toronto.ca/home/covid-19/",
     label: "COVID-19: City of Toronto",
     description: "The City of Toronto's official COVID-19 page. You should be following this."
+  },
+  {
+    url: "https://www.canada.ca/en/department-finance/economic-response-plan.html",
+    label: "Canada’s COVID-19 Economic Response Plan",
+    description: "The federal government's response plan to safeguard the economy."
   }
 ];
